3bfd484502214c027ca1805152dd4a1ccc2033d9
opratapedro-droid <opratapedro@gmail.com>
2026-08-04 09:25:46 -0300

    Código do Desafio de Lógica

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+let xp=200
+let nomeHeroi="FordHudson"
+if (xp<1000){
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Ferro");
+} else if (1000<xp && xp<2000) {
+    console.log("O herói de nome " +nomeHeroi + "está no nível de Bronze");
+} else if (xp>2000 && xp<3000) {
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Ouro")
+} else if (xp>3000 && xp<4000){
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Platina")
+} else if (xp>4000 && xp<5000){
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Ascendente")
+} else if (xp>5000 && xp<6000){
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Imortal")
+} else if (xp>6000){
+    console.log("O herói de nome " +nomeHeroi + " está no nível de Radiante")
+}
